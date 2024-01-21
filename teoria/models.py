e84ba37bd3a5e80a4f41ea884e3a708b3f397c33
classFrancis <121095986+classFrancis@users.noreply.github.com>
2024-01-21 15:56:23 -0300

    actualizacion models

--- teoria/models.py
+++ teoria/models.py
@@ -7,15 +7,28 @@ class Objeto(models.Model):
     tipoObjeto=models.CharField(max_length=150,null=True)
     
 class Conocimiento(models.Model):
-    sujeto=models.ForeignKey('Sujeto', on_delete=models.CASCADE)
-    objeto=models.ForeignKey(Objeto, on_delete=models.CASCADE)
+    sujeto=models.ForeignKey('Sujeto',on_delete=models.CASCADE)
+    objeto=models.ForeignKey(Objeto,on_delete=models.CASCADE)
     aprehendimiento=models.TextField()
-    sujetoComoOjbeto=models.ForeignKey('Suejeto', on_delete=models.CASCADE)
+    sujetoComoObjetoDeConocimiento=models.ForeignKey('Sujeto',on_delete=models.CASCADE)
 
 class Sujeto(models.Model):
+    MASCULINO='M'
+    FEMENINO='F'
+    OPCIONES_SEXO=[
+        (MASCULINO, 'Masculino'),
+        (FEMENINO, 'Femenino'),
+    ]
     nombreSujeto=models.CharField(max_length=150)
     apellidoSujeto=models.CharField(max_length=150)
     edadSujeto=models.IntegerField()
     alturaSujeto=models.DecimalField(max_digits=3,decimal_places=2,null=True)
     etniaSujeto=models.CharField(max_length=150,null=True)
+    sexoBiologicoSujeto=models.CharField(max_length=1, choices=OPCIONES_SEXO)
+    autopercepcionSexual=models.CharField(max_length=150,null=True)
+    profesionSujeto=models.CharField(max_length=150,null=True)
+    partidoPoliticoSujeto=models.CharField(max_length=150,null=True)
+
+    def getSexoBiologico(self):
+        return dict(self.OPCIONES_SEXO)[self.sexoBiologicoSujeto]   
 
